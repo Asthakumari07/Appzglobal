42f184504571420d3b7d3c04364c0a75a3996c99
Piyush Thakur <Piyush@xcadmins-MacBook-Pro.local>
2025-04-28 11:20:47 +0530

    hello2

--- Server/server.js
+++ Server/server.js
@@ -13,21 +13,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-// Path to the build directory
-const buildpath = path.join(__dirname, "../Client/dist");
-
-// Serve static files from the build directory
-app.use(express.static(buildpath));
-
-// Serve static files from the 'assets' folder inside 'dist'
-app.use("/assets", express.static(path.join(buildpath, "assets")));
-
-// Enable CORS for all origins
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
