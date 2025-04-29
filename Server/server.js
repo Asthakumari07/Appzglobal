5e344f648505ee3b81f2bfc2e1ee419af78bb374
Piyush Thakur <Piyush@xcadmins-MacBook-Pro.local>
2025-04-29 18:14:27 +0530

    hhhhhh

--- Server/server.js
+++ Server/server.js
@@ -32,7 +32,7 @@ app.use("/api/admin", adminRoute);
 // Start the server
 const PORT = process.env.PORT || 5000;
 connectDb().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running at port: ${PORT}`);
   });
 });
