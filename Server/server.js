dd93262abefae34757f1842ec644468df46bb571
Piyush Thakur <Piyush@xcadmins-MacBook-Pro.local>
2025-05-06 10:11:06 +0530

    hhh

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const authRouter = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
@@ -9,20 +8,24 @@ const connectDb = require("./utils/db");
 require("dotenv").config();
 const cors = require("cors");
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-// const buildpath = path.join(__dirname, "../Client/dist");
-// app.use(express.static(buildpath));
-// app.use(express.static(path.join(__dirname, "../Client/public")));
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+const app = express();
 
+// Middleware
+app.use(
+  cors({
+    origin: "https://appzglobaltech.com", // Ensure this matches your frontend URL
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files for frontend (if needed)
+const buildpath = path.join(__dirname, "../Client/dist");
+app.use(express.static(buildpath)); // Serves the React build
+
+// Serve static uploads
 app.use("/uploads", express.static("uploads"));
 
 // Routes
@@ -33,6 +36,7 @@ app.use("/api/admin", adminRoute);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
+
 connectDb().then(() => {
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running at port: ${PORT}`);
